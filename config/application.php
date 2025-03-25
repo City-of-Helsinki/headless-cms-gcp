255b1163f033d10133a7962c885a3f1f6c0ee203
Arttu Erkkilä <arttu.erkkila@geniem.com>
2025-03-25 08:34:25 +0200

    remove debug

--- config/application.php
+++ config/application.php
@@ -151,7 +151,6 @@ Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?? true);
 Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DEBUG_LOG', false);
 Config::define('SCRIPT_DEBUG', false);
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 ini_set('display_errors', '0');
 
 /**
